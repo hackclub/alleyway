3717e296823b3bcb0384545d0c8e5e12c40f8124
Alex Ren <41168529+qcoral@users.noreply.github.com>
2025-10-06 14:23:13 -0400

    added yes to cli migrate, also using dotenv instead of vite meta.env imports

--- site/src/auth.ts
+++ site/src/auth.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 import { openAPI } from "better-auth/plugins";
 import { jwt } from "better-auth/plugins";
 import Database from "better-sqlite3";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const auth = betterAuth({
     account: {
@@ -13,13 +15,13 @@ export const auth = betterAuth({
     database: new Database("./sqlite.db"),
     socialProviders: {
         github: {
-            clientId: import.meta.env.GITHUB_CLIENT_ID as string,
-            clientSecret: import.meta.env.GITHUB_CLIENT_SECRET as string,
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
             scope: ["read:user"],
         },
         slack: {
-            clientId: import.meta.env.SLACK_CLIENT_ID as string,
-            clientSecret: import.meta.env.SLACK_CLIENT_SECRET as string,
+            clientId: process.env.SLACK_CLIENT_ID as string,
+            clientSecret: process.env.SLACK_CLIENT_SECRET as string,
             team: "T0266FRGM",
         },
     },
@@ -27,7 +29,7 @@ export const auth = betterAuth({
     debug: true,
     trustedOrigins: [
         "http://localhost:4321",
-        import.meta.env.PUBLIC_PROD_URL as string,
-        (import.meta.env.PUBLIC_BETTER_AUTH_URL as string) || "",
+        process.env.PUBLIC_PROD_URL as string,
+        (process.env.PUBLIC_BETTER_AUTH_URL as string) || "",
     ],
 });
